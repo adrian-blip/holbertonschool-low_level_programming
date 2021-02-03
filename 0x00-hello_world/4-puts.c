6487b644c170f2a3da037b993192044605a25516
adrian-blip <cokugamer102@gmail.com>
2021-02-03 23:28:06 +0000

    task-

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Funcion principal
+ *
+ * Return: 0 (Retorna el valor 0)
+ */
+int main(void)
+{
+char x[60] = "\"Programming is like building a multilingual puzzle";
+puts(x);
+return (0);
+}
