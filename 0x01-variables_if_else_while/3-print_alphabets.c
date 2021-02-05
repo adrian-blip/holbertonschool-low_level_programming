3bf64f03e604840641aa6d4e681c3c72e5292165
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-02-04 18:28:04 -0600

    tasks

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - loops variables
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char alphab;
+
+for (alphab = 'a'; alphab <= 'z'; alphab++)
+{
+putchar(alphab);
+}
+for (alphab = 'A'; alphab <= 'Z'; alphab++)
+{
+putchar(alphab);
+}
+putchar('\n');
+return (0);
+}
+© 2021 GitHub, Inc.
