0953aafad7696f5a5982d0d2c57a2313a70c28cf
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-02-04 18:30:20 -0600

    jkh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Reverse alphabet
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+char NB = 'a';
+
+for (NB = 'z'; NB >= 'a'; NB--)
+putchar(NB);
+
+putchar('\n');
+
+return (0);
+}
+© 2021 GitHub, Inc.
