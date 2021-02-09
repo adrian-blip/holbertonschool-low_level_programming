ea483590d7020255cacb6df3e70205762c3b695d
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-02-09 17:36:48 -0600

    lk

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - last digit
+ *
+ * @n: entry point
+ *
+ * Return: return las digit
+ **/
+
+int print_last_digit(int n)
+{
+int l;
+
+l = n % 10;
+if (l < 0)
+{
+_putchar(-l + 48);
+return (-l);
+}
+else
+{
+_putchar(l + 48);
+return (l);
+}
+}
