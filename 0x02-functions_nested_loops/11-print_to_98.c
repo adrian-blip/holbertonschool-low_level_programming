a9b7f285cb00303cbcf2936ba860e36d653df84b
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-02-09 17:40:52 -0600

    dfg

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_to_98- prints to 98
+ * @n: number
+ * Return:n
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+for (; n > 98; n--)
+printf("%d, ", n);
+printf("%d\n", n);
+}
+else
+{
+for (; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", n);
+}
+}
