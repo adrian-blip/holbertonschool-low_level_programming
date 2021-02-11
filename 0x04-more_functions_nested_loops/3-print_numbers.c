f8466541490371340125b73ea980d2da745765c2
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-02-11 17:46:45 -0600

    f

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,13 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-void print_numbers(void);
+  * print_numbers - function that prints numbers 0-9
+  * @void - no argument
+  */
+void print_numbers(void)
 {
-int o;
+	char c;
 
-  for (o = '0'; o <= '9'; o++)
-    _putchar(o);
-      _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
-
