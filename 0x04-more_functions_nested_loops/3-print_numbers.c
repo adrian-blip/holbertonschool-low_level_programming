74b862e6952a384bd2479e64ea3473abace41037
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-02-11 17:43:05 -0600

    d

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void);
+{
+int o;
+
+  if(o = 0; 0 <= 10; 0++)
+    _putchar(0);
+      _putchar('\n');
+}
