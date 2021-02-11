2c0968f76da6037bec1cee1385e7d3d67f34fe76
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-02-11 17:28:23 -0600

    fd

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+  * _isdigit - function that check if it's a digit
+  * Return: 1 if digit and 0 if anything else
+  */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
