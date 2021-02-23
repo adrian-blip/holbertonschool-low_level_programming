495a00676b41f754fc8aecf0cd1bba61d12a5f90
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-02-22 19:41:21 -0600

    afhgh

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies memory data
+ * @dest: destination
+ * @src: source
+ * @n: bytes
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+
+for (a = 0; a < n; a++)
+dest[a] = src[a];
+return (dest);
+}
