08f22ff2ebd29b21a4fa7cad06f3d6adb7b2a3b3
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-03-19 23:18:15 -0600

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+
+/**
+ *list_len- print the length of a list in a struct
+ *
+ *@h: pointer to head of list
+ *Return:zero
+ */
+
+
+size_t list_len(const list_t *h)
+
+{
+size_t size = 0; /*initialize counter */
+
+while (h != NULL)
+{
+size++;
+h = h->next;
+
+}
+
+return (size);
+}
