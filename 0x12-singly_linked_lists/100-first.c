80f01d3755f32371ab9c27acb8a35e1b475009fd
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-03-19 23:18:40 -0600

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *functionBeforeMain - Function Before Main
+ *
+ *
+ */
+
+
+void __attribute__ ((constructor)) functionBeforeMain()
+{
+
+printf("%s", "You're beat! and yet, you must allow,\n");
+printf("%s", "I bore my house upon my back!\n");
+}
