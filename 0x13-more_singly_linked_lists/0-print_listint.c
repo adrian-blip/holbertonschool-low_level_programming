60bcf08a7aad1417a605595cec2a360b82673a03
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-03-22 20:17:39 -0600

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - h->n print lenht of a  linked list
+ * @h: pointer to node head
+ * Return: quantity of nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+
+{
+
+size_t counter = 0;
+
+while (h != NULL)
+{
+/*printf("%d\n", h->n);*/
+counter++;
+h = h->next;
+}
+return (counter);
+}
