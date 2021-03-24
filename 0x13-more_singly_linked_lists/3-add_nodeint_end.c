592d166f2f51190ae12d14e9821d99367315bb0b
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-03-24 08:29:22 -0600

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,40 +1,31 @@
 #include "lists.h"
-#include <stdio.h>
 /**
- * add_nodeint_end - adds elements at the end of a  linked list
- * @head: pointer to head of rhe list
- *@n: number of elements to add
- * Return: the address of the new node
+ *  * add_nodeint_end - add  a new node to the beggining of the listint_t.
+ * @head: pointer to pointer *head, that point to the structure.
+ * @n: element int
+ * Return: the numbers of nodes
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
-
 {
+	listint_t *new_node = NULL;
+	listint_t *ptr = NULL;
 
-listint_t *new_node;
-listint_t *aux;
-
-new_node = malloc(sizeof(listint_t));
-
-if (new_node == NULL)
-return (NULL);
-
-new_node->n = n;
-new_node->next = NULL;
-if (*head == NULL)
-{
-
-*head = new_node;
-}
-else
-{
-aux = *head;
-while (aux->next != NULL)
-{
-aux = aux->next;
-}
-
-aux->next = new_node;
-}
-return (new_node);
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!*head)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		ptr = *head;
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = new_node;
+	}
+	return (new_node);
 }
