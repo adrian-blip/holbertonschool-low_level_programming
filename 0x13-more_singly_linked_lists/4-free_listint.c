52107cd8887dd80715aff9e663755773e72d4ba3
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-03-24 08:29:51 -0600

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,20 +1,17 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
- * free_listint - functions that free memory allocated
- * @head: pointer to the head oof the list
+ * free_listint- free a listint_t list.
+ * @head: pointer to pointer the first element of listint_t.
+ * Return: void
  */
-
 void free_listint(listint_t *head)
 {
-listint_t *aux;
+	listint_t *ptr = NULL;
 
-while (head != NULL)
-{
-aux = head;
-head = head->next;
-
-free(aux);
-}
+	while (head != '\0')
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
 }
