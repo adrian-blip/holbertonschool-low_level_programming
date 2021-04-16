e2581308344a8d3e7fdb493ee30d23bd1a2dcdc3
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-04-15 21:42:03 -0500

    Update 102-infinite_add.c

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,54 +1,66 @@
-#include "holberton.h"
 /**
- * infinite_add - this functions add two numbers
-(* a blank line
- *@n1: first number to add
- *@n2: second number to add
- *@r: this is the buffer where the function store the result
- *@size_r: this is the size of the buffer
-* Description: this function adds two numbers)?
-(* section header: the header of this function is holberton.h)*
-* Return: this return a char.
-*/
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
+ *
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int n, i, j, k, l, m, add, rest, d1, d2;
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-	l = k = rest =  0;
-	for (i = 0; n1[i] != '\0'; i++)
-		;
-	for (j = 0; n2[j] != '\0'; j++)
-		;
-	if (i + 2 > size_r || j + 2 > size_r)
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
 		return (0);
-	i--, j--;
-	while (i >= 0 || j >= 0)
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
 	{
-		d1 = 0;
-		d2 = 0;
-		if (i >= 0)
-			d1 = n1[i--] - '0';
-		if (j >= 0)
-			d2 = n2[j--] - '0';
-		add = d1 + d2 + rest;
-		if (add > 9)
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
 		{
-			rest = 1;
-			add = add - 10;
+			carry = 1;
+			sum -= 10;
 		}
 		else
-			rest = 0;
-		r[k++] = (add + '0');
+			carry = 0;
+		r[i] = sum + '0';
+		i++;
+		rl++;
 	}
-	if (rest == 1)
-		r[k++] = (1 + '0');
-	m = k,	k--;
-	for (l = 0; l < k; l++, k--)
+	if (carry > 0)
 	{
-		n = r[k];
-		r[k] = r[l];
-		r[l] = n;
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
 	}
-	r[m] = '\0';
+	i = tmpl = 0;
+	while (i <= rl)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
+		i++;
+	}
+	i = 0;
+	while (i < tmpl)
+	{
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+		i++;
+	}
 	return (r);
 }
