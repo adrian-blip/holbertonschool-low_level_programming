9124d8c37a71b3b732a29e659c9926bfbf2f46ad
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-05-02 17:59:18 -0500

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,42 +1,40 @@
 #include "lists.h"
 
 /**
- *add_dnodeint_end - adds a new node at the beginning of a dlistint_t list.
- *
- *@head: head
- *
- *@n: node
- *
- *Return: new node
+ * add_dnodeint_end - adds a new node to the end of list
+ * @head: double pointer to head of node
+ * @n: node data
+ * Return: new node
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *add_end, *tmp;
+dlistint_t *temp_node, *new_node;
 
-	tmp = *head;
+if (head == NULL)
+return (NULL);
 
-	if (head == NULL)
-		return (0);
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = NULL;
 
-	add_end =  malloc(sizeof(dlistint_t));
-	if (add_end == NULL)
-		return (0);
+temp_node = *head;
 
-	add_end->next = NULL;
-	add_end->n = n;
+if (*head == NULL)
+{
+*head = new_node;
+new_node->prev = NULL;
+return (new_node);
+}
 
-	if (*head == NULL)
-		(*head) = add_end;
+while (temp_node->next != NULL)
+{
+temp_node = temp_node->next;
+}
 
-	else
-	{
-		while (tmp->next != NULL)
-		{
-			tmp = tmp->next;
-		}
-			tmp->next = add_end;
-			add_end->prev = tmp;
-	}
-	return (add_end);
+temp_node->next = new_node;
+new_node->prev = temp_node;
+
+return (new_node);
 }
